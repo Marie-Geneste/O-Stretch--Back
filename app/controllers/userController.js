404f2b8e41844528d328726c99e330f0e2ffd022
Marie-Geneste <marie.e.geneste@gmail.com>
2023-02-22 16:24:10 +0100

    Création de la base de donnée dans postgre et ajout des seeds, corrections des bugs - refacto usercontroller avec le jwt

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -2,6 +2,7 @@ const { User } = require("../models");
 
 const emailValidator = require("email-validator");
 const bcrypt = require('bcrypt');
+const jsonwebtoken = require('jsonwebtoken');
 
 
 const userController = {
@@ -27,111 +28,156 @@ const userController = {
             return res.status(400).json({ errorMessage: "Format d'email non valide" });
         }
 
-    // Comme à partir d'ici on contacte un service extérieur qui nous renvoie une promesse (l'appel à la DB)
-    // on utilise le try / catch pour pouvoir gérer un éventuel rejet de la promesse avec une erreur
-    try {
-      // Vérification que l'email sur lequel l'utilisateur souhaite s'enregistrer n'est pas déjà utilisé dans la DB
-        const alreadyExistingUser = await User.findOne({
+        // Comme à partir d'ici on contacte un service extérieur qui nous renvoie une promesse (l'appel à la DB)
+        // on utilise le try / catch pour pouvoir gérer un éventuel rejet de la promesse avec une erreur
+        try {
+        // Vérification que l'email sur lequel l'utilisateur souhaite s'enregistrer n'est pas déjà utilisé dans la DB
+            const alreadyExistingUser = await User.findOne({
+                where: {
+                    email
+                }
+            });
+
+            if(alreadyExistingUser) {
+                return res.status(400).json({ errorMessage: `L'email ${email} est déjà utilisé` });
+            }
+
+            // Vérification du couple mdp / confirmation de mdp
+            if(password !== passwordConfirm) {
+                return res.status(400).json({ errorMessage: `La confirmation de mot de passe ne correspond pas au mot de passe renseigné` });
+            }
+
+            // Hash du mot de passe pour qu'il ne soit plus en clair
+            const saltRounds = 10; // 10 représente le nombre de "round" de complexité pour générer le salt
+            // On génère d'abord un salt qui va permettre de faire en sorte que deux mdp identiques ne donnent pas le meme hash en sortie
+            const salt = await bcrypt.genSalt(saltRounds);
+            // Ensuite seulement on peut hasher le mdp avec le salt généré (qui sera du coup toujours différent et UNIQUE)
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            // On stocke l'utilisateur en DB avec toutes ses infos
+            const newUser = await User.create({
+                name,
+                email,
+                password: hashedPassword
+            });
+
+            const payload = {
+                user: {
+                    id: newUser.id,
+                },
+            };
+        
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "1h",
+            });
+        
+            res.cookie("jwt", token, {
+                httpOnly: true,
+                secure: true,
+                maxAge: 3600000,
+                sameSite: "none",
+            });
+        
+            res.status(201).json({
+            message: "Utilisateur créé avec succès !",
+            user: {
+                id: newUser.id,
+                name: newUser.name,
+                email: newUser.email,
+            },
+            });
+
+            } catch (error) {
+                console.log(error);
+
+                res.status(500).json({ errorMessage: "Erreur serveur" });
+            }
+    },
+
+    async handleLoginFormSubmission(req, res) {
+        // On récupère  les champs du formulaire
+        const { email, password } = req.body;
+
+        // On vérifie que tous les champs du form sont remplis sinon renvois d'une erreur
+        if( !email || !password ) {
+            return res.status(400).json({ errorMessage: "Veuillez remplir tous les champ" });
+        }
+
+        // On véfifie que le user qui tente de se connecter existe bien en DB
+        try {
+        const existingUser = await User.findOne({
             where: {
-                email
+            email
             }
         });
 
-        if(alreadyExistingUser) {
-            return res.status(400).json({ errorMessage: `L'email ${email} est déjà utilisé` });
+        // Si le user n'existe pas, on renvoie un message d'erreur
+        if(!existingUser) {
+            return res.status(400).json({ errorMessage: "Email ou mot de passe incorrect" });
         }
 
-        // Vérification du couple mdp / confirmation de mdp
-        if(password !== passwordConfirm) {
-            return res.status(400).json({ errorMessage: `La confirmation de mot de passe ne correspond pas au mot de passe renseigné` });
+        // Si il existe, on compare le mdp récupéré du form de connexion avec le hash enregistré dans la DB
+        const hashedPassword = existingUser.password;
+
+        // On utilise la méthode de bcrypt dédiée pour comparé le mdp du form avec celui qu'on a récup en DB de notre user existant, qui est censé correspondre
+        const passwordValidation = await bcrypt.compare(password, hashedPassword);
+
+        // Si le mdp est erroné, on stop tout et on envoie un message à l'utilisateur.
+        if (!passwordValidation) {
+            return res.status(400).json({ errorMessage: "Email ou mot de passe incorrect" });
         }
 
-        // Hash du mot de passe pour qu'il ne soit plus en clair
-        const saltRounds = 10; // 10 représente le nombre de "round" de complexité pour générer le salt
-        // On génère d'abord un salt qui va permettre de faire en sorte que deux mdp identiques ne donnent pas le meme hash en sortie
-        const salt = await bcrypt.genSalt(saltRounds);
-        // Ensuite seulement on peut hasher le mdp avec le salt généré (qui sera du coup toujours différent et UNIQUE)
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // On stocke l'utilisateur en DB avec toutes ses infos
-        await User.create({
-            name,
-            email,
-            password: hashedPassword
+        const payload = {
+            user: {
+                id: existingUser.id,
+            },
+        };
+    
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "1h",
+        });
+    
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 3600000,
+            sameSite: "none",
+        });
+    
+        res.status(200).json({
+            message: "Connexion réussie !",
+            user: {
+                id: existingUser.id,
+                name: existingUser.name,
+            }
         });
 
-        res.redirect('/login');
+        // // On enregistre le user dans la session (dans une première version)
+        // // VERSION AMELIOREE POSSIBLE: on pourrait stocker dans la session uniquement l'id du user
+        // // et faire un appel en DB pour récupérer les infos dont on a besoin dans un middleware dédié uniquement sur les routes qui en ont besoin
+        // req.session.user = {
+        //     id: existingUser.id,
+        //     firstname: existingUser.firstname,
+        //     lastname: existingUser.lastname,
+        //     email: existingUser.email,
+        //     role: existingUser.role
+        // };
+
+        
 
         } catch (error) {
-            console.log(error);
+        console.log(error);
 
-            return res.status(500).render("500");
+        res.status(500).json({ errorMessage: "Erreur serveur" });
         }
-},
+    },
 
-  async handleLoginFormSubmission(req, res) {
-    // On récupère  les champs du formulaire
-    const { email, password } = req.body;
-
-    // On vérifie que tous les champs du form sont remplis sinon renvois d'une erreur
-    if( !email || !password ) {
-        return res.status(400).json({ errorMessage: "Veuillez remplir tous les champ" });
+    logout(req, res) {
+        // On supprime le user de la session
+        req.session.user = null;
+        // et on renvoie une réponse JSON indiquant que l'utilisateur a été déconnecté
+        res.json({ message: "Utilisateur déconnecté" });
     }
-
-    // On véfifie que le user qui tente de se connecter existe bien en DB
-    try {
-      const existingUser = await User.findOne({
-        where: {
-          email
-        }
-      });
-
-      // Si le user n'existe pas, on renvoie un message d'erreur
-      if(!existingUser) {
-        return res.status(400).json({ errorMessage: "Email ou mot de passe incorrect" });
-      }
-
-      // Si il existe, on compare le mdp récupéré du form de connexion avec le hash enregistré dans la DB
-      const hashedPassword = existingUser.password;
-
-      // On utilise la méthode de bcrypt dédiée pour comparé le mdp du form avec celui qu'on a récup en DB de notre user existant, qui est censé correspondre
-      const passwordValidation = await bcrypt.compare(password, hashedPassword);
-
-      // Si le mdp est erroné, on stop tout et on envoie un message à l'utilisateur.
-      if (!passwordValidation) {
-        return res.status(400).json({ errorMessage: "Email ou mot de passe incorrect" });
-      }
-
-      // On enregistre le user dans la session (dans une première version)
-      // VERSION AMELIOREE POSSIBLE: on pourrait stocker dans la session uniquement l'id du user
-      // et faire un appel en DB pour récupérer les infos dont on a besoin dans un middleware dédié uniquement sur les routes qui en ont besoin
-      req.session.user = {
-        id: existingUser.id,
-        firstname: existingUser.firstname,
-        lastname: existingUser.lastname,
-        email: existingUser.email,
-        role: existingUser.role
-      };
-
-    
-      res.redirect("/");
-
-    } catch (error) {
-      console.log(error);
-
-      return res.status(500);
-    }
-
-// ################## LOGOUT ##################
-
-  },
-
-  logout(req, res) {
-    // On supprime le user de la session
-    req.session.user = null;
-    // et on redirige vers la home
-    res.redirect("/");
-  }
 };
 
 module.exports = userController;
