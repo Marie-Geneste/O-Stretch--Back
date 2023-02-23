399fa0b45c820441a7f589abbe30c727b647a6aa
Fred Meziere <Fredzem@outlook.fr>
2023-02-23 13:59:09 +0100

    Modification du fichier userController suite à bug de pull/merge

--- app/controllers/stretchController.js
+++ app/controllers/stretchController.js
@@ -8,6 +8,7 @@ const stretchController = {
             const stretches = await Stretch.findAll();
             res.status(200).json(stretches);
 
+    // Permet d'intercepter les potentielles erreurs lors de la récupération des étirements      
         } catch (error) {
             console.log(error);
             return res.status(500).json({ error: "Internal server error" });
