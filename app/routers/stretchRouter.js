2efdf0688c768758ba5798a7d9ca8eb7e86a861f
Lubolivia <ludovictorval@yahoo.com>
2023-02-27 09:32:33 -0500

    routes stretch

--- app/routers/stretchRouter.js
+++ app/routers/stretchRouter.js
@@ -8,6 +8,17 @@ const router = express.Router();
 // récupération des stretch
 router.get('/',  stretchController.getAllStretches);
 
+// création d'un stretch
+router.post('/stretches', stretchController.getAllStretches);
+
+// récupération d'un stretch par son id
+router.get('/stretches/:id', stretchController.getStretchById);
+
+// mise à jour d'un stretch par son id
+router.put('/stretches/:id', stretchController.updateStretchById);
+
+// suppression d'un stretch par son id
+router.delete('/stretches/:id', stretchController.deleteStretchById);
 
 // on exporte le router 
 module.exports = router;
