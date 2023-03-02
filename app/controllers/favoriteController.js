a2275ef36077af6e48669288bfb228df1db0d4c1
Fred Meziere <85186423+FredMeziere@users.noreply.github.com>
2023-03-02 12:15:31 +0100

    Ajout routes pour les favorites (#32)
    * Ajout du middleware pour supprimer un étirement des favoris de l'utilisateur
    
    * Ajout routes pour les favorites

--- app/controllers/favoriteController.js
+++ app/controllers/favoriteController.js
@@ -1,4 +1,4 @@
-const {UserStretch} = require('../models');
+const { UserStretch } = require('../models');
 
 const favoriteController = {
     getAllFavorites: async (req, res) => {
@@ -6,11 +6,11 @@ const favoriteController = {
         const userId = req.token.sub;
         try {
             const favorites = await UserStretch.findOne({
-                where: {user_id: userId}
+                where: { user_id: userId }
             });
             res.status(200).json(favorites);
 
-        // Permet de d'indique que le serveur a rencontré un problème inattendu qui l'empêche de répondre à la requête.         
+            // Permet de d'indique que le serveur a rencontré un problème inattendu qui l'empêche de répondre à la requête.         
         } catch (error) {
             console.log(error);
             return res.status(500).json({ error: "Internal server error" });
@@ -44,8 +44,33 @@ const favoriteController = {
         }
     },
 
-};
 
+    deleteFavoriteStretch: async (req, res) => {
+        try {
+            const userId = req.token.sub;
+            const stretchId = req.params.id;
+
+            // Vérifiez si le favori existe pour cet utilisateur
+            const favoriteStretch = await UserStretch.findOne({
+                where: { user_id: userId, stretch_id: stretchId }
+            });
+
+            if (!favoriteStretch) {
+                return res.status(404).json({ message: 'Favorite not found' });
+            }
+
+            // Supprimez le favori pour cet utilisateur
+            await favoriteStretch.destroy();
+
+            return res.status(204).send();
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: 'Error deleting favorite' });
+        }
+    }
+
+
+};
 
 
 module.exports = favoriteController;
