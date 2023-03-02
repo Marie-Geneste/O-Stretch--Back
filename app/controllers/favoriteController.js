3057dcb4dacc4dbef9c83cda8b16cb1e07d64a53
Marie-Geneste <113341643+Marie-Geneste@users.noreply.github.com>
2023-03-02 16:11:55 +0100

    Fix de la fonction getAllFavorites (#34)
    * Fix de la fonction getAllFavorites

--- app/controllers/favoriteController.js
+++ app/controllers/favoriteController.js
@@ -1,21 +1,24 @@
-const { UserStretch } = require('../models');
+const { Stretch } = require("../models");
+const UserStretch = require("../models/UserStretch");
+
 
 const favoriteController = {
     getAllFavorites: async (req, res) => {
-        // On récupère la liste des favoris depuis la DB à partir du model Sequelize
         const userId = req.token.sub;
         try {
-            const favorites = await UserStretch.findOne({
+            const favorites = await UserStretch.findAll({
                 where: { user_id: userId }
             });
-            res.status(200).json(favorites);
-
-            // Permet de d'indique que le serveur a rencontré un problème inattendu qui l'empêche de répondre à la requête.         
+            const favoriteTable = favorites.map(favori => favori.stretch_id);
+            const promises = favoriteTable.map(stretchFavorite => Stretch.findByPk(stretchFavorite));
+            const favoritesList = await Promise.all(promises);
+            res.status(200).json(favoritesList);
         } catch (error) {
             console.log(error);
             return res.status(500).json({ error: "Internal server error" });
         }
     },
+    
 
     addOneFavorite: async (req, res) => {
         try {
--- app/models/User.js
+++ app/models/User.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("./sequelize-client");
 
+
 class User extends Model {}
 
 User.init({
