e2265d468de8e4e670e4498806e47a88381c524e
Fred Meziere <Fredzem@outlook.fr>
2023-03-02 18:53:02 +0100

    Ajout du coté back pour le formulaire de contact qui sera disponible en v2. Ajout du controller/router, manque a les relier à l'index

---
+++ app/routers/contactRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// on importe nos controllers
+const contactController = require('../controllers/contactController');
+
+const router = express.Router();
+
+
+router.get('/contact', contactController.contact);
+router.post('/contact', contactController.contact);
+
+module.exports = router;
