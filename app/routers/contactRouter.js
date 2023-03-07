361cc6ca6f4ef353879b812ddde8c654506da029
Fred Meziere <85186423+FredMeziere@users.noreply.github.com>
2023-03-07 09:52:35 +0100

    Ajout d'un fichier de seeding pour une nouvelle table contenant les informations conseils (#38)
    * Ajout du middleware pour supprimer un étirement des favoris de l'utilisateur
    
    * Ajout routes pour les favorites
    
    * Ajout du coté back pour le formulaire de contact qui sera disponible en v2. Ajout du controller/router, manque a les relier à l'index
    
    * Ajout d'un seedingv2 pour l'ajout des conseils qui s'affichent à l'utilisateur

--- app/routers/contactRouter.js
+++ app/routers/contactRouter.js
@@ -5,7 +5,6 @@ const contactController = require('../controllers/contactController');
 
 const router = express.Router();
 
-
 router.get('/contact', contactController.contact);
 router.post('/contact', contactController.contact);
 
