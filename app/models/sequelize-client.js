f0d5687e63c72593273896c887072b2f9f561a42
Andy-Vasseur <beaugosswwe@live.fr>
2023-03-09 11:37:38 +0100

    change sequelize URL

--- app/models/sequelize-client.js
+++ app/models/sequelize-client.js
@@ -3,7 +3,7 @@ require("dotenv/config");
 const { Sequelize } = require('sequelize');
 
 // Ceci est une instance de connexion à la BDD Postgres (c'est notre "client")
-const sequelize = new Sequelize(process.env.PG_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     define: {
         timestamps: false
